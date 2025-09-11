3007b9cb82fe2afd01f3d5afc307c69be6259d41
Matheus Martinelli <matheusmartinelli19@gmail.com>
2025-09-10 22:22:03 -0300

    Extensions recommendations

---
+++ src/main/java/com/exemplo/demo/models/User.java
@@ -0,0 +1,72 @@
+package com.exemplo.demo.models;
+
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    public interface CreateUser {}
+    public interface UpdateUser {}
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
+    private String username;
+    
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+    private String password;
+
+    // private List<Task> tasks = new ArrayList<Task>();
+
+    @Override
+    public boolean equals(Object object) {
+        if(object == this) {
+            return true;
+        }
+        if(!(object instanceof User)) {
+            return false;
+        }
+        User user = (User) object;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) 
+        && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+}
